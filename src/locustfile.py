3ce8523126aefdc4e6b1a642b959ff5c3375c74d
Toffy Dee <codycc1004@googlemail.com>
2023-03-08 18:22:47 +0100

    feat: finishes POC of testing resource download, browser interaction and API queries for dev and prod

--- src/locustfile.py
+++ src/locustfile.py
@@ -1,5 +1,6 @@
 import os, json
 from time import perf_counter, sleep
+from dotenv import load_dotenv
 
 from bs4 import BeautifulSoup
 
@@ -15,23 +16,30 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
 
 
+load_dotenv()
+
+
 def get_execution_time(start_time):
     return int((perf_counter() - start_time) * 1000)
 
 
+def url_from_domain(domain):
+    return 'https://www.{}'.format(domain)
+
+
 #TODO: fetch all resources
-class HttpRequestLoad(HttpUser):
+class HttpRequestLoader(HttpUser):
 
-    fixed_count = 0
+    wait_time = between(1,5)
 
-    @task
-    def load_all_js_and_css(self):
+    fixed_count = 4
+
+
+    def load_all_resources(self, main_url, stage):
 
         try:
         
             start_time = perf_counter()
-            
-            main_url = self.environment.host
 
             response = self.client.get(main_url, catch_response=True)
 
@@ -54,28 +62,40 @@ class HttpRequestLoad(HttpUser):
             #summarizes all downloads as single request
             events.request.fire(
                 request_type="GET", 
-                name="load_all_js_css", 
+                name=f"{stage}_load_all_resources", 
                 response_time=load_time, 
                 response_length=0 #TODO
             )
 
         except Exception as e:
-
+            
             exception_type = type(e)
             
             # Report the request duration to Locust
             events.request.fire(
                 request_type="GET", 
-                name="load_all_js_css", 
+                name=f"{stage}_load_all_resources", 
                 response_time=0, 
                 response_length=0,
                 exception=exception_type
             )
 
+    
+    @task
+    def dev_load_all_resources(self):
+        url = url_from_domain(os.getenv("DOMAIN_DEV"))
+        self.load_all_resources(url, 'dev')
 
-class BrowserInteract(HttpUser):
+    
+    @task
+    def prod_load_all_resources(self):
+        url = url_from_domain(os.getenv("DOMAIN_PROD"))
+        self.load_all_resources(url, 'prod')
 
-    wait_time = constant(5)
+
+class BrowserInteractor(HttpUser):
+
+    wait_time = between(1,5)
 
     fixed_count = 2
 
@@ -87,21 +107,19 @@ class BrowserInteract(HttpUser):
             # options.add_argument('--headless')
             self.driver = webdriver.Chrome(options=options)
 
+
     def on_stop(self):
         if self.driver:
             self.driver.quit()
 
-
     #TODO improve replace
-    def extract_domain_from_host_url(self):
-        url = self.environment.host
-        url = url.replace('http:', '').replace('https:', '').replace('www.', '').replace('/', '')
-        return url
+    def extract_domain_from_url(self, url):
+        return url.replace('http:', '').replace('https:', '').replace('www.', '').replace('/', '')
 
 
-    def add_cookies(self):
+    def add_cookies(self, url):
 
-        domain_name = self.extract_domain_from_host_url()
+        domain_name = self.extract_domain_from_url(url)
 
         cookie_file = '{}/../data/cookies_{}.json'.format(
             os.path.dirname(os.path.abspath(__file__)),
@@ -128,6 +146,10 @@ class BrowserInteract(HttpUser):
         self.driver.delete_all_cookies()
 
 
+    def move_to_default_page(self):
+        self.driver.get(os.getenv('DEFAULT_URL'))
+
+
     def load_login_page(self, url):
 
         start_time = perf_counter()
@@ -141,24 +163,24 @@ class BrowserInteract(HttpUser):
             )
         )
 
-        self.driver.get_screenshot_as_file("./screenshot_title_presence.png")
+        # self.driver.get_screenshot_as_file("./screenshot_title_presence.png")
 
         # gets all buttons of page with title
-        logout_buttons = WebDriverWait(self.driver, 120).until(
+        buttons = WebDriverWait(self.driver, 30).until(
             EC.visibility_of_all_elements_located(
-                (By.CSS_SELECTOR, "button")
+                (By.CSS_SELECTOR, "button.css-5uatcg.edgvbvh5")
             )
         )
 
-        self.driver.get_screenshot_as_file("./screenshot_buttons.png")
+        # self.driver.get_screenshot_as_file("./screenshot_buttons.png")
 
         loaded_expected_element = False
 
-        for button in logout_buttons:
+        for button in buttons:
             if button.text == 'Login':
-                WebDriverWait(self.driver, 120).until(
+                WebDriverWait(self.driver, 15).until(
                     EC.element_to_be_clickable(
-                        (By.CSS_SELECTOR, "button")
+                        button
                     )
                 )
                 loaded_expected_element = True
@@ -174,13 +196,11 @@ class BrowserInteract(HttpUser):
 
         # self.driver.implicitly_wait(10)
 
-        
-
         start_time = perf_counter()
 
         self.driver.get(url)
 
-        self.add_cookies()
+        self.add_cookies(url)
 
         # waits for title
         WebDriverWait(self.driver, 15).until(
@@ -189,32 +209,27 @@ class BrowserInteract(HttpUser):
             )
         )
 
-        sleep(3)
-
-        self.driver.get_screenshot_as_file("./screenshot_title_presence.png")
+        # self.driver.get_screenshot_as_file("./screenshot_title_presence.png")
 
         loaded_expected_element = False
 
         for i in range(10):
-            print('iteration: ', i)
 
             # gets all buttons of page with title
-            logout_buttons = WebDriverWait(self.driver, 20).until(
+            buttons = WebDriverWait(self.driver, 30).until(
                 EC.visibility_of_all_elements_located(
-                    (By.CSS_SELECTOR, "button")
+                    # (By.CSS_SELECTOR, "button")
+                    (By.CSS_SELECTOR, "button.css-5uatcg.edgvbvh10")
                 )
             )
 
-            print('buttons:\n', logout_buttons)
+            # self.driver.get_screenshot_as_file("./screenshot_buttons.png")
 
-            self.driver.get_screenshot_as_file("./screenshot_buttons.png")
-
-            for button in logout_buttons:
+            for button in buttons:
                 
                 try:
-                    print('button: ', button.get_attribute('outerHTML'))
                     if 'Logout' in button.text:
-                        WebDriverWait(self.driver, 25).until(
+                        WebDriverWait(self.driver, 15).until(
                             EC.element_to_be_clickable(
                                 button
                             )
@@ -222,8 +237,8 @@ class BrowserInteract(HttpUser):
                         loaded_expected_element = True
                         break
                 except Exception as e:
-                    raise
                     print('exception: ', e)
+                    # raise
                     pass
 
             if loaded_expected_element:
@@ -237,24 +252,23 @@ class BrowserInteract(HttpUser):
         return get_execution_time(start_time)
 
 
-    @task
-    def interact_on_login_page(self):
+    def interact_on_login_page(self, url, stage):
 
         try:
-
-            url = self.environment.host
             
             load_time = self.load_login_page(url)
 
             # Report the request duration to Locust
             events.request.fire(
                 request_type="GET", 
-                name="load_login_page", 
+                name=f"{stage}_load_login_page", 
                 response_time=load_time, 
                 response_length=0 #TODO
             )
 
             # TODO: collect interaction metrics
+
+            self.move_to_default_page()
 
         except Exception as e:
 
@@ -263,27 +277,25 @@ class BrowserInteract(HttpUser):
             # Report the request duration to Locust
             events.request.fire(
                 request_type="GET", 
-                name="load_login_page", 
+                name=f"{stage}_load_login_page", 
                 response_time=0, 
                 response_length=0,
                 exception=exception_type
             )
 
+            raise
 
-    @task
-    def interact_on_main_page(self):
+
+    def interact_on_main_page(self, url, stage):
 
         try:
-
-            url = self.environment.host
             
             load_time = self.load_main_page(url)
-            # load_time = self.load_login_page(url)
 
             # Report the request duration to Locust
             events.request.fire(
                 request_type="GET", 
-                name="load_main_page", 
+                name=f"{stage}_load_main_page", 
                 response_time=load_time, 
                 response_length=0 #TODO
             )
@@ -291,23 +303,230 @@ class BrowserInteract(HttpUser):
             # TODO: collect interaction metrics
 
             self.clear_cache()
+            self.move_to_default_page()
 
         except Exception as e:
-            # raise
-
+           
             exception_type = type(e)
             
             # Report the request duration to Locust
             events.request.fire(
                 request_type="GET", 
-                name="load_main_page", 
+                name=f"{stage}_load_main_page", 
                 response_time=0, 
                 response_length=0,
                 exception=exception_type
             )
 
-class APITester():
+            raise
+
+
+    @task
+    def dev_login_page(self):
+        url = url_from_domain(os.getenv('DOMAIN_DEV'))
+        self.interact_on_login_page(url, stage='dev')
+
+    @task
+    def prod_login_page(self):
+        url = url_from_domain(os.getenv('DOMAIN_PROD'))
+        self.interact_on_login_page(url, stage='prod')
+
+    @task
+    def dev_main_page(self):
+        url = url_from_domain(os.getenv('DOMAIN_DEV'))
+        self.interact_on_main_page(url, stage='dev')
+
+    @task
+    def prod_main_page(self):
+        url = url_from_domain(os.getenv('DOMAIN_PROD'))
+        self.interact_on_main_page(url, stage='prod')
+
+
+class APITester(HttpUser):
+    
+    fixed_count = 4
+
+    wait_time = between(1,5)
+
+    def get_api_key(self):
+        return os.getenv("API_KEY")
+
+    def get_endpoint_url(self, tablename):
+        return f'{self.environment.host}/api/v1/{tablename}?limit=5'.replace('//api', '/api')
+    
+    def call_endpoint(self, base_url, table_name, query, api_key, test_name):
+
+        endpoint = f'{base_url}/api/v1/{table_name}?{query}'.replace('//api', '/api')
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Accept-Profile": "demo"
+        }
+        self.client.get(endpoint, headers=headers, name=test_name)
+
     
     @task
-    def api():
-        pass
+    def dev_boalf_5(self):
+
+        api_key = os.getenv('API_KEY_DEV')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_DEV'))
+
+        table_name = 'boalf'
+
+        query = 'limit=5'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API dev_boalf_5')
+
+
+    @task
+    def dev_boalf_50000(self):
+
+        api_key = os.getenv('API_KEY_DEV')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_DEV'))
+
+        table_name = 'boalf'
+
+        query = 'and=(settlement_start.gte.2023-03-01T09:00:00+00:00,settlement_start.lte.2023-03-03T09:00:00+00:00)&order=available_at.desc&limit=50000'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API dev_boalf_50000')
+
+    
+    @task
+    def prod_boalf_5(self):
+
+        api_key = os.getenv('API_KEY_PROD')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_PROD'))
+
+        table_name = 'boalf'
+
+        query = 'limit=5'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API prod_boalf_5')
+
+
+    @task
+    def prod_boalf_50000(self):
+
+        api_key = os.getenv('API_KEY_PROD')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_PROD'))
+
+        table_name = 'boalf'
+
+        query = 'and=(settlement_start.gte.2023-03-01T09:00:00+00:00,settlement_start.lte.2023-03-03T09:00:00+00:00)&order=available_at.desc&limit=50000'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API prod_boalf_50000')
+
+
+    @task
+    def dev_b1770_5(self):
+
+        api_key = os.getenv('API_KEY_DEV')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_DEV'))
+
+        table_name = 'b1770'
+
+        query = 'limit=5'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API dev_b1770_5')
+
+
+    @task
+    def dev_b1770_50000(self):
+
+        api_key = os.getenv('API_KEY_DEV')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_DEV'))
+
+        table_name = 'b1770'
+
+        query = 'and=(settlement_start.gte.2023-03-01T09:00:00+00:00,settlement_start.lte.2023-03-03T09:00:00+00:00)&order=available_at.desc&limit=50000'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API dev_b1770_50000')
+
+    
+    @task
+    def prod_b1770_5(self):
+
+        api_key = os.getenv('API_KEY_PROD')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_PROD'))
+
+        table_name = 'b1770'
+
+        query = 'limit=5'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API prod_b1770_5')
+
+
+    @task
+    def prod_b1770_50000(self):
+
+        api_key = os.getenv('API_KEY_PROD')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_PROD'))
+
+        table_name = 'b1770'
+
+        query = 'and=(settlement_start.gte.2023-03-01T09:00:00+00:00,settlement_start.lte.2023-03-03T09:00:00+00:00)&order=available_at.desc&limit=50000'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API prod_b1770_50000')
+
+
+    @task
+    def dev_pn_5(self):
+
+        api_key = os.getenv('API_KEY_DEV')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_DEV'))
+
+        table_name = 'pn'
+
+        query = 'limit=5'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API dev_pn_5')
+
+
+    @task
+    def dev_pn_50000(self):
+
+        api_key = os.getenv('API_KEY_DEV')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_DEV'))
+
+        table_name = 'pn'
+
+        query = 'and=(settlement_start.gte.2023-03-01T09:00:00+00:00,settlement_start.lte.2023-03-03T09:00:00+00:00)&order=available_at.desc&limit=50000'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API dev_pn_50000')
+
+    
+    @task
+    def prod_pn_5(self):
+
+        api_key = os.getenv('API_KEY_PROD')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_PROD'))
+
+        table_name = 'pn'
+
+        query = 'limit=5'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API prod_pn_5')
+
+
+    @task
+    def prod_pn_50000(self):
+
+        api_key = os.getenv('API_KEY_PROD')
+
+        base_url = url_from_domain(os.getenv('DOMAIN_PROD'))
+
+        table_name = 'pn'
+
+        query = 'and=(settlement_start.gte.2023-03-01T09:00:00+00:00,settlement_start.lte.2023-03-03T09:00:00+00:00)&order=available_at.desc&limit=50000'
+
+        self.call_endpoint(base_url, table_name, query, api_key, 'API prod_pn_50000')
